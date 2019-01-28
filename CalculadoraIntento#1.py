e743e7c8d0677073892a25dd303ef61db9b4bf3b
Evelin <evelin992009@gmail.com>
2019-01-27 21:17:51 -0500

    boton C y arregladas operaciones X + - /

--- CalculadoraIntento#1.py
+++ CalculadoraIntento#1.py
@@ -10,7 +10,7 @@ Resultado=0 #ahise va guardar todo uwu
 #cuadro de texto
 numeroPantalla=StringVar()
 cuadroN=Entry(mFrame,textvariable=numeroPantalla)#se asocia conla pantalla
-cuadroN.grid(row=1,column=1,padx=10,pady=10,columnspan=4)#columnspan para que salgan bien los botones ya que ocupa las 4 espacios
+cuadroN.grid(row=1,column=1,padx=10,pady=10,columnspan=5)#columnspan para que salgan bien los botones ya que ocupa las 4 espacios
 cuadroN.config(background="black",justify="center", fg="pink")
 #teclado funciones
 
@@ -20,22 +20,29 @@ def numeroPulsado(num):
 
     if BorrarPantalla != False:
         numeroPantalla.set(num)
-
         BorrarPantalla = False#BOrrar la pantalla
 
-    if Operaciones != "":
-        numeroPantalla.set(num)#si eol usurio puilsa suma no lo concatena, oseqa los borre
-        Operaciones=""#PARA QUE SEPUES DE BORRARLO EL SIGUIENTE SI SEA CAPAZ DE MIOSTRAR LOS DIGITOS DEL SIGUIENTE NUMERO COMPLETO
+    else:
 
-    else :
-        numeroPantalla.set(numeroPantalla.get()+num)
+        numeroPantalla.set(numeroPantalla.get() + num)
 
+
+def borrar(num):
+    global Operaciones
+    global BorrarPantalla
+    global Resultado
+    Resultado =int(num)
+    Operaciones="borrar"
+    BorrarPantalla = True
+    numeroPantalla.set(Resultado)
+    #borar=true
 
 
 def Sumar(num):
     global Operaciones
     global Resultado
     global BorrarPantalla
+
     Resultado+=int(num)#para que lo tome como numerp
     Operaciones="sumar"
     BorrarPantalla=True
@@ -51,31 +58,32 @@ def Restar(num):
     global BorrarPantalla
     global num1
     global contador_resta
+
     if contador_resta == 0:
 
         num1 = int(num)
 
-        resultado = num1
+        Resultado= num1
 
     else:
 
         if contador_resta == 1:
 
-            resultado = num1 - int(num)
+            Resultado = num1 - int(num)
 
         else:
 
-            resultado = int(resultado) - int(num)
+            resultado = int(Resultado) - int(num)
 
-        numeroPantalla.set(resultado)
+        numeroPantalla.set(Resultado)
 
         resultado = numeroPantalla.get()
 
     contador_resta = contador_resta + 1
 
-    operacion = "resta"
+    Operaciones = "resta"
 
-    reset_pantalla = True
+    BorrarPantalla= True
 
 contador_multi=0
 
@@ -84,31 +92,31 @@ def Multi(num):
     global Resultado
     global BorrarPantalla
     global num1
-    global contador_resta
+    global contador_multi
     if contador_multi == 0:
         num1 = int(num)
 
-        resultado = num1
+        Resultado = num1
 
     else:
 
         if contador_multi == 1:
 
-            resultado = num1 * int(num)
+            Resultado = num1 * int(num)
 
         else:
 
-            resultado = int(resultado) * int(num)
+            Resultado = int(Resultado) * int(num)
 
-        numeroPantalla.set(resultado)
+        numeroPantalla.set(Resultado)
 
-        resultado = numeroPantalla.get()
+        Resultado = numeroPantalla.get()
 
     contador_multi = contador_multi + 1
 
-    operacion = "multiplicacion"
+    Operaciones = "multiplicacion"
 
-    reset_pantalla = True
+    BorrarPantalla = True
 
 contador_divi=0
 
@@ -127,25 +135,25 @@ def Divi(num):
 
         if contador_multi == 1:
 
-            resultado = num1 / float(num)
+            Resultado = num1 / int(num)
 
         else:
 
-            resultado = float(resultado) / float(num)
+            Resultado = int(Resultado) / int(num)
 
-        numeroPantalla.set(resultado)
+        numeroPantalla.set(Resultado)
 
-        resultado = numeroPantalla.get()
+        Resultado = numeroPantalla.get()
 
     contador_divi = contador_divi + 1
 
-    operacion = "division"
+    Operaciones = "division"
 
-    reset_pantalla = True
+    BorrarPantalla = True
 
 
 #el resultado :
-def ERUSLTADO():
+def ELRESULTADO():
     global Resultado
     global Operaciones
     global contador_divi
@@ -158,42 +166,45 @@ def ERUSLTADO():
        Resultado=0#PARA QUE AL FINAL SE QUEDE EN CERO LA VARIABLE
 
     elif Operaciones=="restar":
-        numeroPantalla.set( Resultado - int(numeroPantalla.get()))  # COGE LO QUE YA LLEVA SUMAO Y LE DA EL RESULTADO COMPLETO
+        numeroPantalla.set( int(Resultado)-int(numeroPantalla.get()))  # COGE LO QUE YA LLEVA SUMAO Y LE DA EL RESULTADO COMPLETO
         Resultado = 0  # PARA QUE AL FINAL SE QUEDE EN CERO LA VARIABLE
         contador_resta=0
 
     elif Operaciones == "mltiplicacion":
-        numeroPantalla.set( Resultado * int(numeroPantalla.get()))  # COGE LO QUE YA LLEVA SUMAO Y LE DA EL RESULTADO COMPLETO
+        numeroPantalla.set( int(Resultado)*int(numeroPantalla.get()))  # COGE LO QUE YA LLEVA SUMAO Y LE DA EL RESULTADO COMPLETO
         Resultado = 0  # PARA QUE AL FINAL SE QUEDE EN CERO LA VARIABLE
         contador_multi = 0
     elif Operaciones == "division":
-        numeroPantalla.set(
-            Resultado / int(numeroPantalla.get()))  # COGE LO QUE YA LLEVA SUMAO Y LE DA EL RESULTADO COMPLETO
+        numeroPantalla.set( int(Resultado)/int(numeroPantalla.get()))  # COGE LO QUE YA LLEVA SUMAO Y LE DA EL RESULTADO COMPLETO
         Resultado = 0  # PARA QUE AL FINAL SE QUEDE EN CERO LA VARIABLE
         contador_divi = 0
+    elif Operaciones=="borrar":
+        numeroPantalla.set("")
+        Resultado=0
 
+    #fila1
 
-#fila1
-
-B7=Button(mFrame,text="7",width=3,command=lambda :numeroPulsado("7"))
+B7=Button(mFrame,text="7",width=3,command=lambda:numeroPulsado("7"))
 B7.grid(row=2,column=1)
-B8=Button(mFrame,text="8",width=3,command=lambda :numeroPulsado("8"))
+B8=Button(mFrame,text="8",width=3,command=lambda:numeroPulsado("8"))
 B8.grid(row=2,column=2)
-B9=Button(mFrame,text="9",width=3,command=lambda :numeroPulsado("9"))
+B9=Button(mFrame,text="9",width=3,command=lambda:numeroPulsado("9"))
 B9.grid(row=2,column=3)
-BD=Button(mFrame,text="/",width=3, command=lambda: Divi(numeroPantalla.get()))
+BD=Button(mFrame,text="/",width=3, command=lambda:Divi(numeroPantalla.get()))
 BD.grid(row=2,column=4)
+BB=Button(mFrame,text="C",width=3, command=lambda:borrar(numeroPantalla.get()))
+BB.grid(row=2,column=5)
 
 #fila 2
 
 
-B4=Button(mFrame,text="4",width=3,command=lambda :numeroPulsado("4"))#lambda pra que no aparezca error
+B4=Button(mFrame,text="4",width=3,command=lambda:numeroPulsado("4"))#lambda pra que no aparezca error
 B4.grid(row=3,column=1)
-B5=Button(mFrame,text="5",width=3,command=lambda :numeroPulsado("5"))
+B5=Button(mFrame,text="5",width=3,command=lambda:numeroPulsado("5"))
 B5.grid(row=3,column=2)
-B6=Button(mFrame,text="6",width=3,command=lambda :numeroPulsado("6"))
+B6=Button(mFrame,text="6",width=3,command=lambda:numeroPulsado("6"))
 B6.grid(row=3,column=3)
-BM=Button(mFrame,text="X",width=3, command=lambda: Multi(numeroPantalla.get()))
+BM=Button(mFrame,text="X",width=3, command=lambda:Multi(numeroPantalla.get()))
 BM.grid(row=3,column=4)
 
 
@@ -212,9 +223,9 @@ BR.grid(row=4,column=4)
 
 B0=Button(mFrame,text="0",width=3,command=lambda :numeroPulsado("0"))
 B0.grid(row=5,column=1)
-BP=Button(mFrame,text=".",width=3,command=lambda :numeroPulsado(","))
+BP=Button(mFrame,text=".",width=3,command=lambda :numeroPulsado("."))
 BP.grid(row=5,column=2)
-BI=Button(mFrame,text="=",width=3,command=lambda:ERUSLTADO())
+BI=Button(mFrame,text="=",width=3,command=lambda:ELRESULTADO())
 BI.grid(row=5,column=3)
 BS=Button(mFrame,text="+",width=3, command=lambda: Sumar(numeroPantalla.get()) )#get para pasar el numero en pantalla
 BS.grid(row=5,column=4)
